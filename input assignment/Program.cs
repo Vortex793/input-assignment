74c571120992f382249ce1f7872616ff75cdff00
339821175 <339821175@2260I-L29-015>
2026-02-11 13:13:25 -0500

    part 3 complete

--- input assignment/Program.cs
+++ input assignment/Program.cs
@@ -27,31 +27,42 @@
             //Console.WriteLine(money + "! I really hope that's per hour and not per year!");
 
             //Part 2
-            string firstName, lastName, login;
-            int id;
-            double grade, average;
-            Console.Write("First name: ");
-            firstName = Console.ReadLine();
-            Console.Write("Last name: ");
-            lastName = Console.ReadLine();
-            Console.Write("Grade (9-12): ");
-            grade = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Student ID: ");
-            id = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Login: ");
-            login = Console.ReadLine();
-            Console.Write("Average: ");
-            average = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Your information:");
-            Console.WriteLine("\tLogin:\t" + login);
-            Console.WriteLine("\tID:\t" + id);
-            Console.WriteLine("\tName:\t" + firstName + " " + lastName);
-            Console.WriteLine("\tAverage:" + average + "%");
-            Console.WriteLine("\tGrade:\t" + grade);
+            //string firstName, lastName, login;
+            //int id;
+            //double grade, average;
+            //Console.Write("First name: ");
+            //firstName = Console.ReadLine();
+            //Console.Write("Last name: ");
+            //lastName = Console.ReadLine();
+            //Console.Write("Grade (9-12): ");
+            //grade = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Student ID: ");
+            //id = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Login: ");
+            //login = Console.ReadLine();
+            //Console.Write("Average: ");
+            //average = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Your information:");
+            //Console.WriteLine("\tLogin:\t" + login);
+            //Console.WriteLine("\tID:\t" + id);
+            //Console.WriteLine("\tName:\t" + firstName + " " + lastName);
+            //Console.WriteLine("\tAverage:" + average + "%");
+            //Console.WriteLine("\tGrade:\t" + grade);
 
 
+            //Part 3
+            String name;
+            int age, plusYears, minusYears;
+        
+            Console.Write("What is your name?: ");
 
+            name = Console.ReadLine();
 
+            Console.Write("Hi " + name + " how old are you?: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            plusYears = age + 5;
+            minusYears = age - 5;
+            Console.WriteLine("Did you know that you will be " + plusYears + " in five years, and you were " + minusYears + " five years ago");
 
 
         }
